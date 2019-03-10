87e3c88fa6b4a027544cedb42d4ab0c0b6ae17d8
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-03-10 15:41:30 -0500

    layed out components and screens

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default class App extends React.Component {
+import WelcomeScreen from './screens/WelcomeScreen'
+import AuthScreen from './screens/AuthScreen'
+import MapScreen from './screens/MapScreen'
+import SwipeScreen from './screens/SwipeScreen'
+import LikesScreen from './screens/LikesScreen'
+import SettingsScreen from './screens/SettingsScreen'
+
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -17,5 +24,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+  }
+})
+
+export default App
