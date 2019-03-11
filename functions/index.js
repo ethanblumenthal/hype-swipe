82f7799aea2732fde669a5c46e45705f15c411ce
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-03-11 05:22:30 -0500

    created google cloud functions to handle user management

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,8 @@ const admin = require('firebase-admin')
 const functions = require('firebase-functions')
 const serviceAccount = require('./service_account.json')
 const createUser = require('./create_user')
-const requestOneTimePassword = require('./request_one_time_password')
-const verifyOneTimePassword = require('./verify_one_time_password')
+const requestOneTimePassword = require('./request_password')
+const verifyOneTimePassword = require('./verify_password')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
